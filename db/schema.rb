4956ebe391c13e7da58595f6b96af368ceffa8f4
kato <user@MacBook-Air.local>
2020-07-09 14:50:06 +0900

    db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_07_08_090758) do
-ActiveRecord::Schema.define(version: 2020_07_08_085759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "name"
-    t.string "password_digest"
-  end
 
   create_table "blogs", force: :cascade do |t|
     t.string "title", null: false
@@ -28,3 +21,13 @@ ActiveRecord::Schema.define(version: 2020_07_08_085759) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
