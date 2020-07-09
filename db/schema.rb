61ba7f37a893e73b75016eeced6b387054280301
kato <user@MacBook-Air.local>
2020-07-09 15:01:58 +0900

    コンフリクト解消

--- db/schema.rb
+++ db/schema.rb
@@ -12,15 +12,8 @@
 
 ActiveRecord::Schema.define(version: 2020_07_08_090758) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "name"
-    t.string "password_digest"
-  end
 
   create_table "blogs", force: :cascade do |t|
     t.string "title", null: false
@@ -28,3 +21,13 @@ ActiveRecord::Schema.define(version: 2020_07_08_090758) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
